76d7b5f8c0f76883f1462885148a576ea32489af
undefinedhuman <alexander.padberg@code.berlin>
2023-08-03 00:44:46 +0200

    feat: typescript

--- eslint.config.js
+++ eslint.config.js
@@ -1,15 +1,20 @@
-module.exports = [
-    {
-        parserOptions: {
-            ecmaVersion: 2018,
-        },
-        files: [ "**/*.vue" ],
-        extends: ["eslint:recommended"],
-        plugins: [
-            "eslint-tailwind-groupify"
-        ],
-        rules: {
-            "eslint-tailwind-groupify/sort": "warn",
-        },
-    }
-]
+module.exports = {
+    parserOptions: {
+        ecmaVersion: 2018,
+    },
+    files: [ "**/*.vue" ],
+    extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended-type-checked',
+        'plugin:@typescript-eslint/stylistic-type-checked'
+    ],
+    parser: '@typescript-eslint/parser',
+    plugins: [
+        '@typescript-eslint',
+        "eslint-tailwind-groupify"
+    ],
+    rules: {
+        "eslint-tailwind-groupify/sort": "warn",
+    },
+    root: true
+}
